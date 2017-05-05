29d17edf1fbe11a67bbb2266ac9b605093b9a0cd
Jaspreet Singh <j.singh@students.uu.nl>
2017-05-05 16:17:21 +0200

    init

---
+++ INFOMAA_Assignment/INFOMAA_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace INFOMAA_Assignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
