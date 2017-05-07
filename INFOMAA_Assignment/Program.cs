0594b66fd7188f23909e95ce83d82363cac40681
Jaspreet Singh <j.singh@students.uu.nl>
2017-05-07 18:21:36 +0200

    updated test scenarios

--- INFOMAA_Assignment/Program.cs
+++ INFOMAA_Assignment/Program.cs
@@ -6,11 +6,15 @@ namespace INFOMAA_Assignment
     {
         public static void Main(string[] args)
         {
+            // angles to test: All divisors of 360 > 10: 12 15 18 20 24 30 36 40 45 60 72 90
+            //                                        k: 30, 24, 20, 18, 12, 10, 9, 8, 6, 5, 4
+
+            int[] ks = { 30, 24, 20, 18, 12, 10, 9, 8, 6, 5, 4 };
             Torus torus = new Torus(100, 100);
-            ActionSet actionSet = new ActionSet(6);
+            ActionSet actionSet = new ActionSet(30);
             Distribution distribution = new Distribution(0.5, new Random(1));
 
-            Game game = new Game(torus, 5, actionSet, 3, 1, -1, 10, distribution, 3600);
+            Game game = new Game(torus, 10, actionSet, 3, 1, -10, 10, distribution, 1000000);
             game.Start();
             Console.ReadLine();
         }
