9b88102834c5c9a89c4b1d631adceadf35564116
D.J. Stekelenburg <d.j.stekelenburg@students.uu.nl>
2017-05-09 23:31:32 +0200

    Improved logging

--- INFOMAA_Assignment/Game.cs
+++ INFOMAA_Assignment/Game.cs
@@ -4,47 +4,47 @@ using System.Linq;
 
 namespace INFOMAA_Assignment
 {
-  public class Game
-  {
-    Logger _logger;
-    readonly Torus _torus;
-    ActionSet _actionSet;
-    Player[] _players;
-
-    readonly int _numPlayers;
-    readonly int _colissionRadius;
-    readonly int _positiveReward;
-    readonly int _negativeReward;
-    readonly int _speed;
-
-    int _clock;
-    int _gameLength;
-
-    public Game(Torus torus, int numPlayers, ActionSet actionSet, int colissionRadius, int positiveReward, int negativeReward, int speed, Distribution distribution, int gameLength)
+    public class Game
     {
-      _clock = 0;
-      _gameLength = gameLength;
+        Logger _logger;
+        readonly Torus _torus;
+        ActionSet _actionSet;
+        Player[] _players;
 
-      _torus = torus;
-      _actionSet = actionSet;
+        readonly int _numPlayers;
+        readonly int _colissionRadius;
+        readonly int _positiveReward;
+        readonly int _negativeReward;
+        readonly int _speed;
 
-      _colissionRadius = colissionRadius;
-      _positiveReward = positiveReward;
-      _negativeReward = negativeReward;
-      _numPlayers = numPlayers;
-      _speed = speed;
+        int _clock;
+        int _gameLength;
 
-      _players = new Player[numPlayers];
-      for(int i = 0; i < numPlayers; i++)
-      {
-        _players[i] = new Player(actionSet, distribution);
-        Random randomService = distribution.GetRandomService();
-        int x = randomService.Next(0, torus.Width);
-        int y = randomService.Next(0, torus.Height);
-        _players[i].SetPosition(new Position(x, y));
-      }
+        public Game(Torus torus, int numPlayers, ActionSet actionSet, int colissionRadius, int positiveReward, int negativeReward, int speed, Distribution distribution, int gameLength)
+        {
+            _clock = 0;
+            _gameLength = gameLength;
 
-      _logger = new Logger(gameLength, actionSet.CleanCopy(), numPlayers, new[]{
+            _torus = torus;
+            _actionSet = actionSet;
+
+            _colissionRadius = colissionRadius;
+            _positiveReward = positiveReward;
+            _negativeReward = negativeReward;
+            _numPlayers = numPlayers;
+            _speed = speed;
+
+            _players = new Player[numPlayers];
+            for (int i = 0; i < numPlayers; i++)
+            {
+                _players[i] = new Player(actionSet, distribution);
+                Random randomService = distribution.GetRandomService();
+                int x = randomService.Next(0, torus.Width);
+                int y = randomService.Next(0, torus.Height);
+                _players[i].SetPosition(new Position(x, y));
+            }
+
+            _logger = new Logger(gameLength, actionSet.CleanCopy(), numPlayers, new[]{
                 torus.Width.ToString(),
                 torus.Height.ToString(),
                 numPlayers.ToString(),
@@ -55,73 +55,73 @@ namespace INFOMAA_Assignment
                 negativeReward.ToString(),
                 $"{distribution.Epsilon:0.000}"
             });
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("{0}-", _logger._parameters);
+            while (_clock < _gameLength)
+            {
+                Console.Write($"\rGametime: {_clock + 1} of {_gameLength} seconds");
+
+                Step();
+            }
+            _logger.Dump();
+        }
+
+        public void Step()
+        {
+            Dictionary<int, List<int>> rewardsPerAction = new Dictionary<int, List<int>>();
+
+            foreach (var action in _actionSet.Keys)
+            {
+                rewardsPerAction.Add(action, new List<int>());
+            }
+
+            for (int i = 0; i < _numPlayers; i++)
+            {
+                int action = _players[i].GetAction();
+                if (action == -1)
+                {
+                    throw new Exception("No suitable action found");
+                }
+                Position next = _torus.NextPosition(_players[i].GetPosition(), _speed, action);
+                bool colission = false;
+
+                for (int j = 0; j < _numPlayers; j++)
+                {
+                    colission |= (i != j && IsCollision(next, _players[j]));
+                }
+
+                if (!colission)
+                {
+                    _players[i].SetPosition(next);
+                    _players[i].AddReward(action, _positiveReward);
+                }
+                else
+                {
+                    _logger.LogCollision(_clock);
+                    _players[i].AddReward(action, _negativeReward);
+                }
+
+                // Initialize list of scores for this action
+                if (!rewardsPerAction.ContainsKey(action))
+                {
+                    rewardsPerAction.Add(action, new List<int>());
+                }
+
+                // Add score to list
+                rewardsPerAction[action].Add(!colission ? _positiveReward : _negativeReward);
+            }
+
+            // Log means of scores per action
+            _logger.LogActionMeans(_clock, rewardsPerAction);
+            _clock++;
+        }
+
+        private bool IsCollision(Position pos, Player player)
+        {
+            return Math.Sqrt((player.GetPosition().X - pos.X) * (player.GetPosition().X - pos.X) + (player.GetPosition().Y - pos.Y) * (player.GetPosition().Y - pos.Y)) < _colissionRadius;
+        }
     }
-
-    public void Start()
-    {
-      Console.WriteLine("{0}-", _logger._parameters);
-      while(_clock < _gameLength)
-      {
-        Console.Write($"\rGametime: {_clock + 1} of {_gameLength} seconds");
-
-        Step();
-      }
-      _logger.Dump();
-    }
-
-    public void Step()
-    {
-      Dictionary<int, List<int>> rewardsPerAction = new Dictionary<int, List<int>>();
-
-      foreach(var action in _actionSet.Keys)
-      {
-        rewardsPerAction.Add(action, new List<int>());
-      }
-
-      for(int i = 0; i < _numPlayers; i++)
-      {
-        int action = _players[i].GetAction();
-        if(action == -1)
-        {
-          throw new Exception("No suitable action found");
-        }
-        Position next = _torus.NextPosition(_players[i].GetPosition(), _speed, action);
-        bool colission = false;
-
-        for(int j = 0; j < _numPlayers; j++)
-        {
-          colission |= (i != j && IsCollision(next, _players[j]));
-        }
-
-        if(!colission)
-        {
-          _players[i].SetPosition(next);
-          _players[i].AddReward(action, _positiveReward);
-        }
-        else
-        {
-          _logger.LogCollision(_clock);
-          _players[i].AddReward(action, _negativeReward);
-        }
-
-        // Initialize list of scores for this action
-        if(!rewardsPerAction.ContainsKey(action))
-        {
-          rewardsPerAction.Add(action, new List<int>());
-        }
-
-        // Add score to list
-        rewardsPerAction[action].Add(!colission ? _positiveReward : _negativeReward);
-      }
-
-      // Log means of scores per action
-      _logger.LogActionMeans(_clock, rewardsPerAction);
-      _clock++;
-    }
-
-    private bool IsCollision(Position pos, Player player)
-    {
-      return Math.Sqrt((player.GetPosition().X - pos.X) * (player.GetPosition().X - pos.X) + (player.GetPosition().Y - pos.Y) * (player.GetPosition().Y - pos.Y)) < _colissionRadius;
-    }
-  }
 }
--- INFOMAA_Assignment/Logger.cs
+++ INFOMAA_Assignment/Logger.cs
@@ -5,124 +5,112 @@ using System.Linq;
 
 namespace INFOMAA_Assignment
 {
-  public class Logger
-  {
-    // Propensities per per t per player
-    // ActionSet[] _propensitiesPerTimeStep;
-
-    private Dictionary<int, List<double>> _meanPerTimeStep = new Dictionary<int, List<double>>();
-
-    int[] _colissionsPerTimeStep;
-
-    int _numSteps;
-    int _numPlayers;
-
-    private ActionSet _actionSet;
-
-    string _hashcode;
-    public string _parameters;
-
-    public Logger(int timeSteps, ActionSet actionSet, int numPlayers, string[] parameters)
+    public class Logger
     {
-      _numSteps = timeSteps;
-      _numPlayers = numPlayers;
-      _actionSet = actionSet;
-      _colissionsPerTimeStep = new int[timeSteps];
+        // Propensities per per t per player
+        // ActionSet[] _propensitiesPerTimeStep;
 
-      foreach(int degrees in _actionSet.Keys)
-      {
-        _meanPerTimeStep.Add(degrees, new List<double>());
-      }
+        private Dictionary<int, List<double>> _meanPerTimeStep = new Dictionary<int, List<double>>();
 
-      _parameters = "";
-      foreach(string parameter in parameters)
-      {
-        _parameters += $"-{parameter}";
-      }
+        int[] _colissionsPerTimeStep;
 
-      _hashcode = DateTime.Now.GetHashCode().ToString("x8");
-    }
+        int _numSteps;
+        int _numPlayers;
 
-    public void LogCollision(int timeStep)
-    {
-      _colissionsPerTimeStep[timeStep]++;
-    }
+        private ActionSet _actionSet;
 
-    public void LogActionMeans(int timeStep, Dictionary<int, List<int>> meanRewardPerAction)
-    {
-      foreach(int action in meanRewardPerAction.Keys)
-      {
-        double sum = 0;
-        foreach(int score in meanRewardPerAction[action])
+        string _hashcode;
+        public string _parameters;
+
+        public Logger(int timeSteps, ActionSet actionSet, int numPlayers, string[] parameters)
         {
-          sum += score;
+            _numSteps = timeSteps;
+            _numPlayers = numPlayers;
+            _actionSet = actionSet;
+            _colissionsPerTimeStep = new int[timeSteps];
+
+            foreach (int degrees in _actionSet.Keys)
+            {
+                _meanPerTimeStep.Add(degrees, new List<double>());
+            }
+
+            _parameters = "";
+            foreach (string parameter in parameters)
+            {
+                _parameters += $"-{parameter}";
+            }
+
+            _hashcode = DateTime.Now.GetHashCode().ToString("x8");
         }
 
-        double mean = 0;
-
-        if(meanRewardPerAction[action].Count > 0)
+        public void LogCollision(int timeStep)
         {
-          mean = sum / meanRewardPerAction[action].Count;
+            _colissionsPerTimeStep[timeStep]++;
         }
-        _meanPerTimeStep[action].Add(mean);
-      }
-    }
 
-    public void Dump()
-    {
-      Console.WriteLine("\nDumping scores per action per time step");
-      DumpScoresPerActionPerTimeStep();
-      Console.WriteLine("Dumping collisions");
-      DumpColissions();
-    }
-
-    private void DumpScoresPerActionPerTimeStep()
-    {
-      string[] entries = new string[_numSteps + 1];
-      entries[0] = CreateHeader(_actionSet);
-
-      for(int i = 1; i <= _numSteps; i++)
-      {
-        List<double> meansPerAction = new List<double>();
-        foreach(KeyValuePair<int, List<double>> kvp in _meanPerTimeStep)
+        public void LogActionMeans(int timeStep, Dictionary<int, List<int>> meanRewardPerAction)
         {
-          meansPerAction.Add(kvp.Value[i - 1]);
+            foreach (int action in meanRewardPerAction.Keys)
+            {
+                double sum = 0;
+                foreach (int score in meanRewardPerAction[action])
+                {
+                    sum += score;
+                }
+
+                double mean = 0;
+
+                if (meanRewardPerAction[action].Count > 0)
+                {
+                    mean = sum / meanRewardPerAction[action].Count;
+                }
+                _meanPerTimeStep[action].Add(mean);
+            }
         }
-        entries[i] += CreateEntry(i, meansPerAction);
-      }
-      File.WriteAllLines(_hashcode + _parameters + "_scores.csv", entries);
-    }
 
-    private string CreateEntry(int timeStep, List<double> meansPerAction)
-    {
-      string entry = $"{timeStep}";
-      foreach(double mean in meansPerAction)
-      {
-        entry += $";{mean:0.000}";
-      }
-      return entry;
-    }
+        public void Dump()
+        {
+            Console.WriteLine("\nDumping collisions and scores per action per time step");
+            DumpAll();
+        }
 
-    private string CreateHeader(ActionSet actionSet)
-    {
-      string header = "time";
-      foreach(KeyValuePair<int, int> kvp in actionSet)
-      {
-        header += $";action {kvp.Key} degrees";
-      }
-      return header;
-    }
+        private void DumpAll()
+        {
+            string[] entries = new string[_numSteps + 1];
+            entries[0] = CreateHeader(_actionSet);
 
+            for (int i = 1; i <= _numSteps; i++)
+            {
+                List<double> meansPerAction = new List<double>();
+                foreach (KeyValuePair<int, List<double>> kvp in _meanPerTimeStep)
+                {
+                    meansPerAction.Add(kvp.Value[i - 1]);
+                }
+                entries[i] += CreateEntry(i, meansPerAction);
+            }
+            File.WriteAllLines(_hashcode + _parameters + "_result.csv", entries);
+        }
 
-    private void DumpColissions()
-    {
-      string[] entries = new string[_numSteps + 1];
-      entries[0] = "step;numColissions\n";
-      for(int i = 1; i < _numSteps + 1; i++)
-      {
-        entries[i] += $"{i};{_colissionsPerTimeStep[i - 1]}";
-      }
-      File.WriteAllLines(_hashcode + _parameters + "_collisions.csv", entries);
+        private string CreateEntry(int timeStep, List<double> meansPerAction)
+        {
+            string entry = $"{timeStep}";
+            foreach (double mean in meansPerAction)
+            {
+                entry += $";{mean:0.000}";
+            }
+            entry += $";{_colissionsPerTimeStep[timeStep - 1]}";
+            return entry;
+        }
+
+        private string CreateHeader(ActionSet actionSet)
+        {
+            string header = "time";
+            foreach (KeyValuePair<int, int> kvp in actionSet)
+            {
+                header += $";action {kvp.Key} degrees";
+            }
+            header += $";collisions";
+            return header;
+        }
     }
-  }
 }
